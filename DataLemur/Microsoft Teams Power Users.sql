e43e17317f440193619d8522f387d5bf220b0454
Emirhan <kisaemirhan57@gmail.com>
2026-02-07 10:43:51 +0300

    Update and rename Microsoft Teams Power Users.sql to Microsoft Teams Power Users.sql

--- DataLemur/Microsoft Teams Power Users.sql
+++ DataLemur/Microsoft Teams Power Users.sql
@@ -1,3 +1,10 @@
+/*
+Problem: Microsoft Teams Power Users
+Dialect: MySql
+Link : https://datalemur.com/questions/teams-power-users
+*/
+
+-- Query 
 SELECT sender_id, COUNT(message_id) AS message_count
 FROM messages
 WHERE sent_date BETWEEN '2022-08-01' AND '2022-08-31'
